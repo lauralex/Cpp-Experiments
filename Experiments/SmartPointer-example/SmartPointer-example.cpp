23a61de531f6cb41144afb9df7042b418a41462a
lauralex <casildaserrano@yahoo.it>
2020-09-22 23:25:54 +0200

    Commented code

--- Experiments/SmartPointer-example/SmartPointer-example.cpp
+++ Experiments/SmartPointer-example/SmartPointer-example.cpp
@@ -4,6 +4,9 @@
 #include <utility>
 
 
+/**
+ * \brief This is a class using a std::string to allocate memory
+ */
 class std_string
 {
 public:
@@ -24,6 +27,9 @@ void std_string::print() const
 }
 
 
+/**
+ * \brief This is a class using a unique pointer to allocate memory
+ */
 class smart_ptr
 {
 public:
@@ -41,14 +47,16 @@ smart_ptr::smart_ptr(const std::string& my_string)
 
 void check_mem()
 {
-	const std_string pex{"hello"};
-	const smart_ptr rex{"neck"};
+	const std_string pex{"hello"}; // Use a std::string to allocate memory
+	const smart_ptr rex{"neck"}; // Use a unique pointer to allocate memory (both will manage memory deallocation!)
 	pex.print();
 }
 
 int main()
 {
+	// Check if there are any memory leaks! (if we use smart pointers, there won't be any!)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	
 	check_mem();
 	return 0;
 }
