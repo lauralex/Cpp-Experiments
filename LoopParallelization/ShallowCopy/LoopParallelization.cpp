875f01c5d7b22743ebef7f4e9bee596eff53779a
lauralex <casildaserrano@yahoo.it>
2020-09-22 12:49:23 +0200

    Added a comment
    Added a little comment about parallelization

--- LoopParallelization/ShallowCopy/LoopParallelization.cpp
+++ LoopParallelization/ShallowCopy/LoopParallelization.cpp
@@ -21,7 +21,8 @@ int main()
 
 	const auto t1 = std::chrono::high_resolution_clock::now();
 
-#pragma loop(ivdep)  // This loop will probably NOT be parallelized
+#pragma loop(ivdep)  // This loop will probably NOT be parallelized (if you add the following line, the parallelization will be forced)
+#pragma loop(hint_parallel(4)) // This will use 4 threads
 	for (auto i = 0; i < size; i++)
 	{
 		array3[i] = array1[i] * static_cast<int>(sqrt(array1[i])) + static_cast<int>(sqrt(array2[i] * 5));
